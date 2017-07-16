5ee0f63d90a7db66aa0fa1b184a2dfa72eec619f
Konstantin <woodprop@yandex.ru>
2017-07-16 16:26:45 +0300

    image_name переименован в image_file

--- application/views/product/product.php
+++ application/views/product/product.php
@@ -3,7 +3,7 @@
     <div class="row">
     <div class="col-12"><h1><?= $product->name ?></h1></div>
     <div class="col-4">
-        <img src="<?= base_url() . 'img/' . $product->image_name ?>" alt="<?= $product->name ?>" class="img-fluid">
+        <img src="<?= base_url() . 'img/' . $product->image_file ?>" alt="<?= $product->name ?>" class="img-fluid">
     </div>
 
     <div class="col-6 bg-faded">
